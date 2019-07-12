583eaefd31459c286f6ffed566f44e54cee42f62
Justin <justindchan@gmail.com>
2019-07-12 09:55:42 -0700

    on chaining map, flatten and reduce section

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -64,13 +64,13 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
     
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var sum = var sum = _.range(0, 1000).reduce(function(sum, x){if (x % 3 === 0 || x % 5 === 0) {return sum + x}});    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(233168);
   });
 
   /*********************************************************************************/
